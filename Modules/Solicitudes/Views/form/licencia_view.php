f19089fc06e88d896a57d6d45ce0c4257af3408f
humbatista <humbatista@gmail>
2023-10-26 20:00:16 -0400

    renuncia

--- Modules/Solicitudes/Views/form/licencia_view.php
+++ Modules/Solicitudes/Views/form/licencia_view.php
@@ -294,8 +294,8 @@
       </form>
       <!-- Fin Modal Borrar Solicitud-->
   
-  <script src="/js/jquery.min.js"></script>
-  <script src="/js/bootstrap.bundle.min.js"></script>
+  <!-- <script src="/js/jquery.min.js"></script>
+  <script src="/js/bootstrap.bundle.min.js"></script> -->
   <script>
       $(document).ready(function(){
   
--- Modules/Solicitudes/Views/header/head.php
+++ Modules/Solicitudes/Views/header/head.php
@@ -39,8 +39,8 @@
     </style>
 
 
-<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
-<link rel="stylesheet" href="<?php echo base_url('/public/fontawesome-free/css/fontawesome.css');?>">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<!-- <link rel="stylesheet" href="<?php //echo base_url('/public/fontawesome-free/css/fontawesome.css');?>"> -->
 <!-- <link href="/open-iconic/font/css/open-iconic-bootstrap.css" rel="stylesheet"> -->
     <!-- <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> -->
     <link rel="stylesheet" href="<?php echo base_url('/public/assets/css/w3.css');?>">
