3e9fd29839adef9f735d3e3a34656ba66806e4e9
humbatista <humbatista@gmail>
2024-03-13 20:20:13 -0400

    modificar modal desactivar

--- Modules/Empleados/Views/form/empleados_view.php
+++ Modules/Empleados/Views/form/empleados_view.php
@@ -1,20 +1,20 @@
 <!-- Page Content -->
 <div class="container" style="margin-top: 20px; margin-left:15%;">
-        <h3 style="text-align: center; margin-bottom: 48px; font-size: 60px;">Listado Colaboradores</h3>
-        <!--Formulario de busqueda -->
-            <div class='row'> 
-                <div class='col-md-5'> 
-                    <form method="get" name="searchForm"action="empleados/" id="searchForm">
-                        <input type="text" name="search" value="<?= $search ?>" placeholder="Buscar">
-                        <input type="submit" id="btnsearch" value="Submit" onclick='document.getElementById("SeachForm").submit();'>
-                    </form>
-                </div>
-                <div class="col-md-5"> 
-                    <a type='button' href='<?php echo base_url('empleados/export');?>' class="btn btn-success">Imprimir Listado</a>
-                </div>
-            </div>
+    <h3 style="text-align: center; margin-bottom: 48px; font-size: 60px;">Listado Colaboradores</h3>
+    <!--Formulario de busqueda -->
+    <div class='row'>
+        <div class='col-md-5'>
+            <form method="get" name="searchForm" action="empleados/" id="searchForm">
+                <input type="text" name="search" value="<?= $search ?>" placeholder="Buscar">
+                <input type="submit" id="btnsearch" value="Submit" onclick='document.getElementById("SeachForm").submit();'>
+            </form>
+        </div>
+        <div class="col-md-5">
+            <a type='button' href='<?php echo base_url('empleados/export'); ?>' class="btn btn-success">Imprimir Listado</a>
+        </div>
+    </div>
 
-        <table class="table table-hover" style="border-collapse: collapse;">
+    <table class="table table-hover" style="border-collapse: collapse;">
         <thead>
             <tr>
                 <td>Nombre</td>
@@ -32,28 +32,28 @@
         <tbody>
             <?php
             foreach ($empleados as $rows) {
-                if($rows['vacaciones']=='SI'){
+                if ($rows['vacaciones'] == 'SI') {
                     $color = '#E57373';
                     $semaforo = 'text-danger';
-                } else{
+                } else {
                     $color = '#FFEE58';
                     $semaforo = 'text-success';
                 }
                 echo "<tr>";
-                echo "<td>". $rows['nombre']. "</td>";
-                echo "<td>". $rows['apellido']. "</td>";
-                echo "<td>". $rows['cedula']. "</td>";
-                echo "<td>". $rows['cargo']. "</td>";
-                echo "<td>". $rows['funcion']. "</td>";
-                echo "<td>". $rows['unidad']. "</td>";
-                echo "<td>". $rows['fecingreso']. "</td>";
-                echo "<td>". $rows['vacaciones']. "</td>";
-                echo "<td><i class='fa fa-circle ".$semaforo."'></i></td>";
+                echo "<td>" . $rows['nombre'] . "</td>";
+                echo "<td>" . $rows['apellido'] . "</td>";
+                echo "<td>" . $rows['cedula'] . "</td>";
+                echo "<td>" . $rows['cargo'] . "</td>";
+                echo "<td>" . $rows['funcion'] . "</td>";
+                echo "<td>" . $rows['unidad'] . "</td>";
+                echo "<td>" . $rows['fecingreso'] . "</td>";
+                echo "<td>" . $rows['vacaciones'] . "</td>";
+                echo "<td><i class='fa fa-circle " . $semaforo . "'></i></td>";
                 echo "<td>
-                        <a href='http://localhost/gestionhumana/empleados/servidor?codigo=".$rows['codigo']."'class='btn  btn-info'>Ver</a>
-                        <a href= 'http://localhost/gestionhumana/empleados/editar?id=".$rows['codigo']."' class='btn  btn-warning' data-id=". $rows['codigo'].">Editar</a>
-                        <a href= 'http://localhost/gestionhumana/empleados/certificado?id=".$rows['codigo']."' class='btn btn-success'>Certificado</a>
-                        <a href= '#' class='btn btn-danger' data-id=". $rows['codigo'].">Desactivar</a>
+                        <a href='http://localhost/gestionhumana/empleados/servidor?codigo=" . $rows['codigo'] . "'class='btn  btn-info'>Ver</a>
+                        <a href= 'http://localhost/gestionhumana/empleados/editar?id=" . $rows['codigo'] . "' class='btn  btn-warning' data-id=" . $rows['codigo'] . ">Editar</a>
+                        <a href= 'http://localhost/gestionhumana/empleados/certificado?id=" . $rows['codigo'] . "' class='btn btn-success'>Certificado</a>
+                        <a href= '#' class='btn btn-danger' data-id=" . $rows['codigo'] . ">Desactivar</a>
                       </td>";
                 echo "</tr>";
             }
@@ -64,7 +64,7 @@
 
     <!-- Paginacion -->
     <div style="margin-top: 10px;">
-    <?= $pager->links() ?>
+        <?= $pager->links() ?>
     </div>
 
 </div>
@@ -75,12 +75,17 @@
     <div class="modal fade" id="obsModal" tabindex="-1" role="dialog" aria-labelledby="obsModal" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Confirmacion desactivar</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</spanF>
+                    </button>
+                </div>
                 <div class="modal-body">
-                    <h1>Desea desactivar este codigo</h1>
+                    <p>Desea desactivar este Colaborador</p>
                 </div>
                 <div class="modal-footer">
                     <input type="hidden" name="id" class="id">
-                    <input type="hidden" name="tipo" value='Inclusion'>
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>
                     <button type="submit" class="btn btn-primary">Si</button>
                 </div>
@@ -88,21 +93,24 @@
         </div>
     </div>
 </form>
+
+
 <script>
     function ImpCertificado() {
         //alert('funciona');
         $.ajax({
-            //url: '<?php //echo base_url('solicitud/reporte/inclusion/export');?>',
-            url: '<?php echo base_url('empleados/export');?>',
+            //url: '<?php //echo base_url('solicitud/reporte/inclusion/export');
+                    ?>',
+            url: '<?php echo base_url('empleados/export'); ?>',
             type: "post",
-            data:{},
+            data: {},
         });
     };
 </script>
 
 <script>
-    $(document).ready(function(){
-        $('.btn-danger').on('click',function(){
+    $(document).ready(function() {
+        $('.btn-danger').on('click', function() {
             var id = $(this).data('id');
             //alert(id)
             $('.id').val(id);
@@ -110,4 +118,3 @@
         });
     });
 </script>
-
